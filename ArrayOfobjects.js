a4ecf8e6bdc0ef392ff196c9b5c3ab13605850f1
AcEdArKwhIt <136297433+AcEdArKwhIt@users.noreply.github.com>
2023-06-13 12:00:06 +0530

    Task files

---
+++ ArrayOfobjects.js
@@ -0,0 +1,16 @@
+let a = [{ name: "Ace" }, { age: 18 },
+{ dob: "07-07-2005" }, { place: "karur" },
+{ bg: "B-" }, { ambition: null },
+{ Number : 9629923925 }, { sport: "Badminton" },
+{ school: "LAEHSS" }, { college: "GPT" }
+];
+console.log(a);
+let i = a;
+for (let i = 0; i <= 9;) {
+    console.log(a[i])
+    i++;
+}
+
+var z=[{},{},{}]
+// let b =a.map(i);
+console.log(z);
